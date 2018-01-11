4562c270df1de3d1ee85e95859cc305751c9dfe3
wff <fei2684281886@QQ.com>
2018-01-11 21:51:16 +0800

    登录页面和侧边栏页面完成

---
+++ public/back/js/common.js
@@ -0,0 +1,69 @@
+/**
+ * Created by Administrator on 2018/1/11.
+ */
+
+$(function () {
+    // 进度条
+    NProgress.configure({showSpinner: false});
+    $(document).ajaxStart(function () {
+        NProgress.start();
+    });
+    $(document).ajaxStop(function () {
+        setTimeout(function () {
+            NProgress.done();
+        }, 1000);
+    });
+
+    // 二级菜单点击显示出来
+    $(".nav-second").on("click", function () {
+        $(this).next().slideToggle();
+    });
+
+    // 侧边栏隐藏与显示 同时 padding-left:0
+    $(".first").on("click" , function() {
+        $(".lt-aside").toggleClass("now");
+        $(".lt-main").toggleClass("now");
+    });
+
+    // 退出显示
+    $(".second").on("click" , function() {
+        // 模态框显示
+        $('#out').modal('show');
+
+        // 不可以在里面注册，否则会被事件覆盖，会连续注册
+    });
+
+    // 给退出按钮注册事件
+    $(".getOut").on("click",function() {
+       //发送ajax 请求退出
+        $.ajax ({
+            type : "get",
+            url : "/employee/employeeLogout",
+            success : function(info) {
+                if (info.success) {
+                    location.href = "login.html";
+                }
+            }
+        })
+    });
+
+    // 检测是否登录
+    // 在非登录页面，发送ajax请求 检测用户是否登录
+    if (location.href.indexOf("login.html") == -1 ) {
+        // 发送ajax请求
+        $.ajax ({
+            type : "get",
+            url : "/employee/checkRootLogin",
+            success : function(info) {
+                // 判断如果是错误 就跳转为登录页面
+                if (info.error == 400 ) {
+                    location.href = "login.html";
+                }
+            }
+        })
+    }
+
+});
+
+
+
--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,20 +1,82 @@
 /**
  * Created by Administrator on 2018/1/11.
  */
-$(function () {
 
+// 校验登录页面
+$(function () {
+    //初始化表单校验
     var $form = $("form");
-    // 初始化表单校验插件
+    console.log($form);
+    // 使用表单校验插件
     $form.bootstrapValidator({
+        // 指定校验时的图标的显示，默认是 bootstrap 的风格
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok-circle',
+            invalid: 'glyphicon glyphicon-remove-circle',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        // 指定校验字段
         fields: {
+            // 校验用户名 对应name表单的name属性
             username: {
-                validators : {
-                    notEmpty : {
-                        message : "用户名不能为空"
+                validators: {
+                    // 不能为空
+                    notEmpty: {
+                        message: "用户名不能为空"
+                    },
+                    callback : {
+                        message : "用户名不存在"
                     }
                 }
             },
-            password: {}
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: "密码不能为空"
+                    },
+                    // 密码长度校验
+                    stringLength: {
+                        min: 6,
+                        message: "密码不能低于六位哦~"
+                    },
+                    callback : {
+                        message : "密码错误"
+                    }
+                }
+            }
         }
-    })
+    });
+    // 注册表单验证成功事件
+    $form.on("success.form.bv" , function(e) {
+        e.preventDefault();
+        // 提交表单验证
+        // console.log(1);
+        // 使用ajax请求后台
+        $.ajax ({
+            type : "post",
+            url : "/employee/employeeLogin",
+            dataType : "json",
+            // 表单序列化 获取了username 和 password
+            data : $form.serialize(),
+            success : function (info) {
+                if (info.success) {
+                    location.href = "index.html";
+                }
+                if (info.error === 1000 ) {
+                    $form.data("bootstrapValidator").updateStatus("username", "INVALID" ,"callback");
+                }
+                if (info.error === 1001 ) {
+                    $form.data("bootstrapValidator").updateStatus("password", "INVALID" ,"callback");
+                }
+            }
+        })
+
+    });
+
+    // 重置表单
+    $("[type='reset']").on("click", function () {
+        $form.data("bootstrapValidator").resetForm();
+
+    });
+
 });
