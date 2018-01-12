82f95629d63980afe0bb3d88d084f0a4ba7e8104
wff <fei2684281886@QQ.com>
2018-01-12 23:47:41 +0800

    所有页面模态框搭建完成

---
+++ public/back/js/first.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2018/1/12.
+ */
+ $(function() {
+    // 模态框出来
+     $(".first_btn").on("click" , function() {
+         $('#first_box').modal('show')
+     })
+ });
---
+++ public/back/js/second.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2018/1/12.
+ */
+$(function() {
+    // 模态框出来
+    $(".second_btn").on("click" , function() {
+        $('#second_box').modal('show')
+    })
+});
